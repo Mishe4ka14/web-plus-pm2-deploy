94bbf92b6786461791be17b78fbba3f87b22d9a4
Mishe4ka14 <mishechka.14@yandex.ru>
2024-05-01 05:42:41 +0300

    fix config for backend

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -17,8 +17,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
-      'post-deploy': 'cd backend && nvm use 18 && npm i && npm run build && pm2 reload app',
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/src`,
+      'post-deploy': 'cd src && nvm use 18 && npm i && npm run build && pm2 reload app',
     },
   },
 };
