aafc0f487119a1356070cd7baa237576c498dfe7
Mishe4ka14 <mishechka.14@yandex.ru>
2024-05-01 22:17:00 +0300

    fix config for frontend

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,27 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REPO,
+  DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'post-deploy': 'cd frontend && . ~/.nvm/nvm.sh && nvm use && npm i && npm run build && pm2 reload app',
+    },
+  },
+};
