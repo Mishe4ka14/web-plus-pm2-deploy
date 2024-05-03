c3112dd936ba55376787ab9a20cb466b854b5f35
Mishe4ka14 <mishechka.14@yandex.ru>
2024-05-03 23:04:08 +0300

    add front

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,18 +1,33 @@
-require('dotenv').config({ path: './.env.deploy' });
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REPO, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF,
+  DEPLOY_REPO_USER_NAME,
+  DEPLOY_REPO_NAME,
 } = process.env;
 
 module.exports = {
+  apps: [
+    {
+      name: "frontend",
+      script : './build/index.html',
+    },
+  ],
+
   deploy: {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
       ref: DEPLOY_REF,
-      repo: DEPLOY_REPO,
+      repo: `https://github.com/${DEPLOY_REPO_USER_NAME}/${DEPLOY_REPO_NAME}.git`,
       path: DEPLOY_PATH,
-      'post-deploy': 'cd frontend && nvm use 16 && npm i && npm run build',
+      "post-deploy":
+          `cd ~/web-plus-pm2-deploy/source/frontend/ && npm i && npm run build`,
     },
   },
 };
