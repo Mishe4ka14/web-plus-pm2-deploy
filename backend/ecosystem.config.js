258953bba41eb901aad7a9747593869b754c9f04
Mishe4ka14 <mishechka.14@yandex.ru>
2024-05-03 04:20:00 +0300

    add env.example

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': 'cd $DEPLOY_PATH/source/backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
 
     },
   },
