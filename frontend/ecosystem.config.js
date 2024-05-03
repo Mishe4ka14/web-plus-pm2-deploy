db417188160def3fdbf07513c456519b482dcf19
Mishe4ka14 <mishechka.14@yandex.ru>
2024-05-03 23:52:18 +0300

    add front

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -7,13 +7,6 @@ const {
 } = process.env;
 
 module.exports = {
-  apps: [
-    {
-      name: "frontend",
-      script : './build/index.html',
-    },
-  ],
-
   deploy: {
     production: {
       user: DEPLOY_USER,
