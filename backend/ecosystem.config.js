427fbac3df04d2ce7bd4c2f04607b9aaefd275d9
Mishe4ka14 <mishechka.14@yandex.ru>
2024-05-03 03:34:31 +0300

    fix all

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,19 +1,18 @@
-require('dotenv').config({ path: './.env.deploy' });
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env.deploy' });
 
 const {
-  DEPLOY_USER,
-  DEPLOY_HOST,
-  DEPLOY_PATH,
-  DEPLOY_REPO,
-  DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
 } = process.env;
 
 module.exports = {
   apps: [{
-    name: 'app',
-    script: './dist/app.js',
+    name: 'api-service',
+    script: 'dist/app.js',
   }],
 
+  // Настройка деплоя
   deploy: {
     production: {
       user: DEPLOY_USER,
@@ -21,8 +20,9 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ${__dirname}/.env.deploy ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/.env`,
-      'post-deploy': 'cd backend && . ~/.nvm/nvm.sh && nvm use 16 && npm i && npm run build && pm2 reload app',
+      'pre-deploy-local': `bash scripts/deployEnv.sh ${DEPLOY_USER}@${DEPLOY_HOST} ${DEPLOY_PATH}`,
+      'post-deploy': 'cd backend && pwd && npm ci && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
+
     },
   },
 };
